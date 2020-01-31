08e964ac5bdd3c2b0b39ea4697db3637ff58b05a
Pavel Parshin <p.parshin@city-mobil.ru>
2020-01-31 15:02:43 +0300

    Added background infinite arping if we cannot delete VIP from old master

--- scripts/orch_vip.sh
+++ scripts/orch_vip.sh
@@ -59,38 +59,47 @@ cmd_vip_del="ifconfig ${interface} down"
 cmd_vip_chk="ifconfig | grep 'inet addr' | grep ${vip}"
 # command for sending gratuitous ARP to announce IP move
 cmd_arp_fix="arping -c 3 -s ${vip} ${gateway}"
+cmd_arp_force_fix="while true; do ${cmd_arp_fix}; sleep 60; done"
+
+vipRemovedFromOldMaster=false
 
 vip_stop() {
-    rc=0
+  rc=0
 
-    # ensure the VIP is removed
-    ssh ${sshOptions} -tt ${sshUser}@${oldMaster} \
-    "[ -n \"\$(${cmd_vip_chk})\" ] && ${cmd_vip_del} && ${cmd_arp_fix} || [ -z \"\$(${cmd_vip_chk})\" ]"
+  # ensure the VIP is removed
+  ssh ${sshOptions} -tt ${sshUser}@${oldMaster} \
+  "[ -n \"\$(${cmd_vip_chk})\" ] && ${cmd_vip_del} && ${cmd_arp_fix} || [ -z \"\$(${cmd_vip_chk})\" ]"
 
-    rc=$?
-    return ${rc}
+  rc=$?
+  return ${rc}
 }
 
 vip_start() {
-    rc=0
+  rc=0
 
-    # ensure the VIP is added
-    # this command should exit with failure if we are unable to add the VIP
-    # if the VIP already exists always exit 0 (whether or not we added it)
-    ssh ${sshOptions} -tt ${sshUser}@${newMaster} \
-     "[ -z \"\$(${cmd_vip_chk})\" ] && ${cmd_vip_add} && ${cmd_arp_fix} || [ -n \"\$(${cmd_vip_chk})\" ]"
-    
-    rc=$?
-    return ${rc}
+  # ensure the VIP is added
+  # this command should exit with failure if we are unable to add the VIP
+  # if the VIP already exists always exit 0 (whether or not we added it)
+  ssh ${sshOptions} -tt ${sshUser}@${newMaster} \
+   "[ -z \"\$(${cmd_vip_chk})\" ] && ${cmd_vip_add} && ${cmd_arp_fix} || [ -n \"\$(${cmd_vip_chk})\" ]"
+  
+  rc=$?
+  return ${rc}
 }
 
 vip_status() {
-    arping -c 2 ${vip}
-    if ping -c 2 -W 1 ${vip}; then
-        return 0
-    else
-        return 1
-    fi
+  arping -c 2 ${vip}
+  if ping -c 2 -W 1 ${vip}; then
+      return 0
+  else
+      return 1
+  fi
+}
+
+# If we do not remove VIP from old master (e.g. some SSH problems),
+# run infinite arping.
+force_arping() {
+  ssh ${sshOptions} -f ${sshUser}@${newMaster} "screen -dmS orch_force_arping_STOP_ME bash -c '${cmd_arp_force_fix}'"
 }
 
 echo "[info] Master is dead, trying to move VIP from old master to a new one"
@@ -99,6 +108,7 @@ echo '[info] Make sure the VIP is not available...'
 if vip_status; then 
     echo '[info] VIP is pingable so try to down it.'
     if vip_stop; then
+        vipRemovedFromOldMaster=true
         echo "[info] ${vip} is removed from ${oldMaster}."
     else
         # We do not treat it as a fatal error.
@@ -109,6 +119,10 @@ fi
 echo '[info] Moving VIP to new master.'
 if vip_start; then
       echo "[info] ${vip} is moved to ${newMaster}."
+      if [ "${vipRemovedFromOldMaster}" = false ]; then
+        echo "[warn] arping is going to run in the background process!"
+        force_arping
+      fi
 else
       echo "[info] Can't add ${vip} on ${newMaster}!" 
       exit 1
